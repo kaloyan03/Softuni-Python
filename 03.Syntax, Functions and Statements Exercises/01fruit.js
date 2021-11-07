10dcba3e97b8b9e62be9cdb897660d451e2a9386
kaloyan03 <87472356+kaloyan03@users.noreply.github.com>
2021-11-07 23:05:44 +0200

    Delete 03.Syntax, Functions and Statements Exercises directory

--- 03.Syntax, Functions and Statements Exercises/01fruit.js
+++
@@ -1,7 +0,0 @@
-  function calculatesFruitPrice(typeOfFruit, weightInGrams, pricePerKilogram) {
-    let weightInKilograms = weightInGrams / 1000;
-    let fruitPrice = weightInKilograms * pricePerKilogram;
-
-    console.log(`I need $${fruitPrice.toFixed(2)} to buy ${weightInKilograms.toFixed(2)} kilograms ${typeOfFruit}.`);
-
-}
