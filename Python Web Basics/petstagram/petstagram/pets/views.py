3a6bc2d5dd3d075c0ca9b9c6458e93c8224fa54d
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2021-12-21 22:49:35 +0200

    Edit and delete functionality implemented :)

--- Python Web Basics/petstagram/petstagram/pets/views.py
+++ Python Web Basics/petstagram/petstagram/pets/views.py
@@ -49,9 +49,6 @@ def create_pet(request):
         return render(request, 'pet_create.html', context)
     else:
         form = CreatePetForm(request.POST)
-        context = {
-            'form': form,
-        }
 
         if form.is_valid():
             form.save()
@@ -59,8 +56,33 @@ def create_pet(request):
 
 
 def edit_pet(request, pk):
-    pass
+    pet = Pet.objects.get(pk=pk)
+    if request.method == 'GET':
+        form = CreatePetForm(initial=pet.__dict__)
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'pet_edit.html', context)
+    else:
+        form = CreatePetForm(request.POST, instance=pet)
+
+        if form.is_valid():
+            form.save()
+            return redirect('pet details', pet.id)
 
 
 def delete_pet(request, pk):
-    pass
+    pet = Pet.objects.get(pk=pk)
+
+    if request.method == 'GET':
+        context = {
+            'pet_name': pet.name,
+        }
+
+        return render(request, 'pet_delete.html', context)
+
+    else:
+        pet.delete()
+        return redirect('list pets')
+
