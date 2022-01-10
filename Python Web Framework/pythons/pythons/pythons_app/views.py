cc0cdf495055839804f4cb6da73eae4fdb6159ab
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-01-10 20:43:50 +0200

    templates advanced lab solution

---
+++ Python Web Framework/pythons/pythons/pythons_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .forms import PythonCreateForm
+from .models import Python
+
+
+# Create your views here.
+def index(req):
+    pythons = Python.objects.all()
+
+    context = {
+        'pythons': pythons,
+        'current_path': req.path,
+    }
+
+    return render(req, 'index.html', context)
+
+
+def create(req):
+    if req.method == 'GET':
+        form = PythonCreateForm()
+        context = {
+            'form': form,
+            'current_path': req.path,
+        }
+        return render(req, 'create.html', context)
+    else:
+        data = req.POST
+        form = PythonCreateForm(data)
+        print(form)
+        if form.is_valid():
+            python = form.save()
+            python.save()
+            return redirect('index')
