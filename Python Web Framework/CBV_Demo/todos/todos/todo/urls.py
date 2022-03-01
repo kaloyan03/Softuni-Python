e0628a7a5670814fad82d28d6c6a18475b505dbb
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-03-01 23:20:12 +0200

    CRUD operations with class-based views

--- Python Web Framework/CBV_Demo/todos/todos/todo/urls.py
+++ Python Web Framework/CBV_Demo/todos/todos/todo/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from todos.todo.views import ListTodos, CreateTodo
+from todos.todo.views import ListTodos, CreateTodo, TodoDetails, DeleteTodo, EditTodo
 
 urlpatterns = (
     path('', ListTodos.as_view(), name='list todos'),
     path('create/', CreateTodo.as_view(), name='create todo'),
+    path('details/<int:pk>', TodoDetails.as_view(), name='todo details'),
+    path('delete/<int:pk>', DeleteTodo.as_view(), name='delete todo'),
+    path('edit/<int:pk>', EditTodo.as_view(), name='edit todo'),
 )
--- Python Web Framework/CBV_Demo/todos/todos/todo/views.py
+++ Python Web Framework/CBV_Demo/todos/todos/todo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 
 from todos.todo.models import Todo
 
@@ -20,6 +20,28 @@ class CreateTodo(CreateView):
     template_name = 'create_todo.html'
     success_url = reverse_lazy('list todos')
     fields = '__all__'
+
+class EditTodo(UpdateView):
+    model = Todo
+    template_name = 'edit_todo.html'
+    success_url = reverse_lazy('list todos')
+    fields = '__all__'
+
+
+
+
+class TodoDetails(DetailView):
+    model = Todo
+    context_object_name = 'todo'
+    template_name = 'todo_details.html'
+
+
+class DeleteTodo(DeleteView):
+    model = Todo
+    template_name = 'todo_delete.html'
+    context_object_name = 'todo'
+    success_url = reverse_lazy('list todos')
+
 
 
 
